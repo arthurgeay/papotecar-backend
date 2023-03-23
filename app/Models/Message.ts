f4848ccb1de3868adb68ce0860d1199d926a1a07
arthurgeay <arthurgeay.contact@gmail.com>
2023-03-23 15:47:15 +0100

    chore: create message model

--- app/Models/Message.ts
+++ app/Models/Message.ts
@@ -1,13 +1,30 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, belongsTo, BelongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Trip from './Trip'
 
 export default class Message extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: string
+
+  @column()
+  public tripId: string
+
+  @column()
+  public userId: string
+
+  @column()
+  public content: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @belongsTo(() => Trip)
+  public trip: BelongsTo<typeof Trip>
 }
