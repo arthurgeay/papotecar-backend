b8fae5a7ccea33626f7f780d972ed8f7e399def7
arthurgeay <arthurgeay.contact@gmail.com>
2023-03-23 15:42:08 +0100

    database: create migration file for message model

---
+++ database/migrations/1679582249762_messages.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'messages'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+      table.uuid('trip_id').unsigned().references('trips.id').notNullable().onDelete('CASCADE')
+      table.uuid('user_id').unsigned().references('users.id').notNullable().onDelete('CASCADE')
+
+      table.text('content').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
